8b5630e972082d2632bc7c503e19d873d55c0a11
Juan Pablo Lastra <juan.pablo.lastra.s@gmail.com>
2023-12-27 12:38:31 -0300

    Added generate_image script to request images to Dall-E

--- Blog/chatgpt.py
+++ Blog/chatgpt.py
@@ -1,30 +1,58 @@
 import os
+import requests
 from openai import OpenAI
 
-def CallChatGpt(systemPrompt, userPrompt, model, temperature, maxTokens):
-  client = OpenAI(
+def CreateOpenAIClient():
+  return OpenAI(
       # This is the default and can be omitted
-      api_key=os.environ['OPENAI_API_KEY'],
+      api_key=os.environ['OPENAI_API_KEY']
   )
 
-  print("Sending prompt to OpenAI API")
+def CallChatGpt(systemPrompt, userPrompt, model, temperature, maxTokens):
+  client = CreateOpenAIClient()
+  print("Sending prompt to ChatGPT model " + model)
   chatCompletion = client.chat.completions.create(
-      messages=[
-        {
-          "role": "system", 
-          "content": systemPrompt
-        },
-        {
-          "role": "user",
-          "content": userPrompt
-        }
-      ],
-      model=model,
-      temperature=temperature,
-      top_p=1.0,
-      frequency_penalty=0.0,
-      presence_penalty=0.0,
-      max_tokens=maxTokens
+    messages=[
+      {
+        "role": "system", 
+        "content": systemPrompt
+      },
+      {
+        "role": "user",
+        "content": userPrompt
+      }
+    ],
+    model=model,
+    temperature=temperature,
+    top_p=1.0,
+    frequency_penalty=0.0,
+    presence_penalty=0.0,
+    max_tokens=maxTokens
   )
   response = chatCompletion.choices[0].message.content
   return response
+
+def CallDallE(prompt, model, size, quality, style, outputFile):
+  client = CreateOpenAIClient()
+  print("Sending prompt to Dall-E model " + model)
+  sizeStr=str(size)+"x"+str(size)
+  response = client.images.generate(
+    prompt=prompt,
+    model=model,
+    size=sizeStr,
+    quality=quality,
+    style=style
+  )
+  url = response.data[0].url
+  DownloadImage(url, outputFile)
+
+def DownloadImage(url, filename):
+  print("Download image from " + url)
+  with open(filename, 'wb') as handle:
+    response = requests.get(url, stream=True)
+    if not response.ok:
+      print(response)
+    for block in response.iter_content(1024):
+      if not block:
+        break
+      handle.write(block)
---
+++ Blog/generate_image.py
@@ -0,0 +1,27 @@
+import os
+import argparse
+import chatgpt
+
+# check arguments
+
+parser = argparse.ArgumentParser("generate_image")
+parser.add_argument("prompt", help="Prompt to generate the image", type=str)
+parser.add_argument("-o","--outputFile", help="File name to write with translated article", type=str)
+parser.add_argument("-m", "--model", help="Model to use", choices=["dall-e-2","dall-e-3"], type=str, default="dall-e-3")
+parser.add_argument("-s", "--size", help="Size of the output image", choices=["512", "1024"], type=str, default="1024")
+parser.add_argument("-q", "--quality", help="Size of the output image", choices=["standard", "hd"], type=str, default="standard")
+parser.add_argument("-st", "--style", help="Style of the output image", choices=["vivid", "natural"], type=str, default="natural")
+args = parser.parse_args()
+
+# if no output file was provided, use a default name
+if args.outputFile is not None:
+  outputFile = args.outputFile
+else:
+  outputFile = args.prompt + "_" + args.model + "_" + args.quality + "_" + args.style + ".md"
+
+# check if output file exists
+if os.path.isfile(outputFile):
+  print("Output file already exists!")
+  exit(1)
+
+chatgpt.CallDallE(args.prompt, args.model, args.size, args.quality, args.style, outputFile)
